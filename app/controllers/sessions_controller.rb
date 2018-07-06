320f1456f64af7bbf3003f1ff9642cc1c290deb8
Dee Race <deerace12@gmail.com>
2018-07-06 16:07:23 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,19 @@
 class SessionsController < ApplicationController
 
-  def new 
-  end 
+  def new
+  end
 
-  def create 
+  def create
     if params[:name].nil? || params[:name].empty?
       redirect_to login_path
-    else 
+    else
       session[:name] = params[:name]
-      redirect_to secrets_path 
-    end 
-  end 
+      redirect_to secrets_path
+    end
+  end
 
-  def destroy 
-    session.delete :name 
+  def destroy
+    session.delete :name
     redirect_to login_path
-  end 
+  end
 end 
